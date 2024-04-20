c3692295013f93cfce4c32e625361757949ffea4
Motasem <motasemali0000@gmail.com>
2024-04-20 09:21:24 +0300

    Refactoring code, building App class
    App class that will run the program
    - method getPosition using geolocation API to get user location

--- script.js
+++ script.js
@@ -10,3 +10,65 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+class App {
+  constructor() {}
+
+  _getPosition() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this._loadMap, function () {
+        alert('Could not found your position!');
+      });
+    }
+  }
+
+  _loadMap(position) {}
+
+  _showForm() {}
+
+  _toggleElevationField() {}
+
+  _newWorkout() {}
+}
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const {
+        coords: { latitude, longitude },
+      } = position;
+
+      const coords = [latitude, longitude];
+      const map = L.map('map').setView(coords, 17); //here 'map' inside the map function is the id of our element we want to render our map on it.
+      //setView 2nd argument, is the zoom level: more value more zoom
+      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords).addTo(map).bindPopup('Oh Hi There').openPopup();
+
+      map.on('click', function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+        const coords = [lat, lng];
+        L.marker(coords)
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Write your popup message')
+          .openPopup();
+      });
+    },
+    function () {
+      alert('Could not found your position!');
+    }
+  );
+}
